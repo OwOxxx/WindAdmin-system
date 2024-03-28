302973b68bb9b6cec3e8953fd8ee57ce15d34b01
ZhouYi <zhouyirabbit@gmail.com>
2024-03-28 14:09:59 +0800

    🚀🚀🚀 drop console and debugger

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,12 @@ import viteSvgIcons, { createSvgIconsPlugin } from "vite-plugin-svg-icons"
 
 import ElementPlus from "unplugin-element-plus/vite"
 
+// 可视化打包分析
+import { visualizer } from "rollup-plugin-visualizer"
+
+// 压缩插件
+import viteCompression from "vite-plugin-compression"
+
 // css原子化UnoCSS
 import UnoCSS from "unocss/vite"
 export default ({ command }: ConfigEnv): UserConfigExport => {
@@ -39,7 +45,25 @@ export default ({ command }: ConfigEnv): UserConfigExport => {
 			//   mockPath: 'mock',
 			//   enable: true,
 			// }),
+			visualizer({ open: true }),
 		],
+		build: {
+			rollupOptions: {
+				plugins: [
+					// viteCompression({
+					// 	verbose: true, // 是否在控制台中输出压缩结果
+					// 	disable: false,
+					// 	threshold: 10240, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
+					// 	algorithm: "gzip", // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
+					// 	ext: ".gz",
+					// 	deleteOriginFile: true, // 源文件压缩后是否删除(我为了看压缩后的效果，先选择了true)
+					// }),
+				],
+			},
+		},
+		esbuild: {
+			drop: ["console", "debugger"], // 去掉console和debugger
+		},
 		server: {
 			open: true,
 			port: 8888,
